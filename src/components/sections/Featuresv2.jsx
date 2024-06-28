eaf7544a0517a3eb8d8468e1e02a908e3ffec0b1
Gabriel <gabrielsouza65115@gmail.com>
2024-06-28 11:41:54 -0300

    fix: Remove linhas de códigos inutilizadas

--- src/components/sections/Featuresv2.jsx
+++ src/components/sections/Featuresv2.jsx
@@ -6,22 +6,8 @@ import FeatureCard from "../cards/FeatureCard";
 import imgRent from "../../assets/imgs/features/niceHouse.jpg";
 import imgValuation from "../../assets/imgs/features/evaluation.jpg";
 import imgDocs from "../../assets/imgs/features/docs.jpg";
-import { Dialog } from "primereact/dialog";
-import "primereact/resources/themes/saga-blue/theme.css";
-import "primereact/resources/primereact.min.css";
-import "primeicons/primeicons.css";
 
 export default function Featuresv2() {
-  const [visible, setVisible] = useState(false);
-  const [modalContent, setModalContent] = useState("");
-  const [modalTitle, setModalTitle] = useState("");
-
-  const handleButtonClick = (title, content) => {
-    setModalTitle(title);
-    setModalContent(content);
-    setVisible(true);
-  };
-
   return (
     <SectionArea className="bg-quinary">
       <SectionHeader
@@ -35,42 +21,19 @@ export default function Featuresv2() {
             img={imgRent}
             title="Venda e Aluguel"
             description="Conte com nossa experiência para vender ou alugar seu imóvel com rapidez e segurança, garantindo a melhor oferta do mercado."
-            onButtonClick={() =>
-              handleButtonClick("Venda e Aluguel", "Conteúdo a ser adicionado")
-            }
           />
           <FeatureCard
             img={imgValuation}
             title="Avaliação"
             description="Realizamos avaliações precisas que captam o verdadeiro valor do seu imóvel, preparando-o para uma oferta ideal."
-            onButtonClick={() =>
-              handleButtonClick("Avaliação", "Conteúdo a ser adicionado")
-
-            }
           />
           <FeatureCard
             img={imgDocs}
             title="Negociação e Documentação"
             description="Facilitamos todo o processo de documentação e conduzimos negociações eficazes para assegurar uma transação tranquila e vantajosa."
-            onButtonClick={() =>
-              handleButtonClick(
-                "Negociação e Documentação",
-                "Conteúdo a ser adicionado"
-
-              )
-            }
           />
         </div>
       </SectionWrapper>
-      <Dialog className=" font-secondFont"
-        header={modalTitle}
-        visible={visible}
-        onHide={() => setVisible(false)}
-        style={{ width: "50vw" }}
-        breakpoints={{ "960px": "75vw", "641px": "90vw" }}
-      >
-        <p className="m-0">{modalContent}</p>
-      </Dialog>
     </SectionArea>
   );
 }
