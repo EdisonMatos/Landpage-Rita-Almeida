2236d591ef20786789eb3ca8ae0d40b77cd4427e
Gabriel <gabrielsouza65115@gmail.com>
2024-07-02 10:59:56 -0300

    fix: Altera a dimensão das imagens e a altura dos espaços onde as logos se encaixam na LogoDisplay

--- src/components/sectionElements/LogoDisplay.jsx
+++ src/components/sectionElements/LogoDisplay.jsx
@@ -8,7 +8,7 @@ const LogoDisplay = ({ images }) => {
           key={index}
           src={image.src}
           alt={image.alt}
-          className="max-h-[40px] w-[45.3%] phone3:max-h-[50px] phone3:w-[39.3%] object-contain tablet1:w-[26.1%] tablet2:max-h-[60px] desktop1:p-[20px] desktop1:w-[28.3%] desktop1:max-h-[80px] desktop3:w-[29.7%]"
+          className="max-h-[70px] w-[45.3%] phone3:max-h-[60px] phone3:w-[39.3%] object-contain tablet1:w-[26.1%] tablet1:max-h-[70px] tablet2:max-h-[80px] desktop1:p-[20px] desktop1:w-[28.3%] desktop1:max-h-[100px] desktop3:w-[29.7%] desktop3:max-h-[120px]"
         />
       ))}
     </div>
