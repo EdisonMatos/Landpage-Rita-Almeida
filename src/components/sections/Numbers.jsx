7eec0fae84223dc94ab0d737a10a68c344e7896f
Edison Matos <edison.matos@live.com>
2024-07-12 16:28:13 -0300

    feat: Escurece paralaxe da secao numbers

--- src/components/sections/Numbers.jsx
+++ src/components/sections/Numbers.jsx
@@ -34,36 +34,42 @@ export default function Numbers() {
         backgroundAttachment: "fixed",
       }}
     >
-      <div className="w-[90%] max-w-[1215px] gap-[46px] flex flex-col text-center desktop1:flex-row justify-between items-center py-[64px] desktop1:py-[96px] text-white [text-shadow:_2px_3px_0_rgb(0_0_0_/_50%)]">
-        <div className="flex flex-col items-center w-[90%] desktop1:w-[30%] ">
-          <h1 className="text-[72px] font-mainFont leading-[76px] font-bold">
-            <MotionDivDownToUp>
-              <AnimatedCounter endValue={4000} />+
-            </MotionDivDownToUp>
-          </h1>
-          <p className=" font-secondFont text-title1 tablet1:text-title3">
-            <MotionDivDownToUp>Seguidores nas redes sociais</MotionDivDownToUp>
-          </p>
-        </div>
-        <div className="flex flex-col items-center w-[90%] desktop1:w-[30%]">
-          <h1 className="text-[72px] font-mainFont leading-[76px] font-bold">
-            <MotionDivDownToUp>
-              <AnimatedCounter endValue={240} />+
-            </MotionDivDownToUp>
-          </h1>
-          <p className="font-secondFont text-title1 tablet1:text-title3">
-            <MotionDivDownToUp>Imóveis vendidos ou alugados</MotionDivDownToUp>
-          </p>
-        </div>
-        <div className="flex flex-col items-center w-[90%] desktop1:w-[30%]">
-          <h1 className="text-[72px] font-mainFont leading-[76px] font-bold">
-            <MotionDivDownToUp>
-              <AnimatedCounter endValue={10} />+
-            </MotionDivDownToUp>
-          </h1>
-          <p className="font-secondFont text-title1 tablet1:text-title3">
-            <MotionDivDownToUp>Anos de Experiência</MotionDivDownToUp>
-          </p>
+      <div className="w-full flex justify-center bg-black bg-opacity-50">
+        <div className="w-[90%] max-w-[1215px] gap-[46px] flex flex-col text-center desktop1:flex-row justify-between items-center py-[64px] desktop1:py-[96px] text-white [text-shadow:_2px_3px_0_rgb(0_0_0_/_50%)]">
+          <div className="flex flex-col items-center w-[90%] desktop1:w-[30%] ">
+            <h1 className="text-[72px] font-mainFont leading-[76px] font-bold">
+              <MotionDivDownToUp>
+                <AnimatedCounter endValue={4000} />+
+              </MotionDivDownToUp>
+            </h1>
+            <p className=" font-secondFont text-title1 tablet1:text-title3">
+              <MotionDivDownToUp>
+                Seguidores nas redes sociais
+              </MotionDivDownToUp>
+            </p>
+          </div>
+          <div className="flex flex-col items-center w-[90%] desktop1:w-[30%]">
+            <h1 className="text-[72px] font-mainFont leading-[76px] font-bold">
+              <MotionDivDownToUp>
+                <AnimatedCounter endValue={240} />+
+              </MotionDivDownToUp>
+            </h1>
+            <p className="font-secondFont text-title1 tablet1:text-title3">
+              <MotionDivDownToUp>
+                Imóveis vendidos ou alugados
+              </MotionDivDownToUp>
+            </p>
+          </div>
+          <div className="flex flex-col items-center w-[90%] desktop1:w-[30%]">
+            <h1 className="text-[72px] font-mainFont leading-[76px] font-bold">
+              <MotionDivDownToUp>
+                <AnimatedCounter endValue={10} />+
+              </MotionDivDownToUp>
+            </h1>
+            <p className="font-secondFont text-title1 tablet1:text-title3">
+              <MotionDivDownToUp>Anos de Experiência</MotionDivDownToUp>
+            </p>
+          </div>
         </div>
       </div>
     </div>
